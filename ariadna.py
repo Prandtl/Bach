424517f908ba3bec8772d430ec3aa527d1765615
Prandtl <vasiliy.uchanev@prandtl.co>
2017-05-16 18:43:09 +0500

    working sample

---
+++ ariadna.py
@@ -0,0 +1,69 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import cm
+import pylab
+import re
+
+filename = "example.out"
+
+def getTestData(h):
+    def testFunction(point):
+        x = point[0]
+        y = point[1]
+        x_sq = np.square(x)
+        second_comp = np.square(x - y)
+        result = x_sq + second_comp
+        return result
+    def numerical_diff_x(point):
+        #(f2-f0)/2h
+        x = point[0]
+        y = point[1]
+        f0 = testFunction((x - h, y))
+        f2 = testFunction((x + h, y))
+        dx = (f2 - f0)/(2 * h)
+        return dx
+    def numerical_diff_y(point):
+        #(f2-f0)/2h
+        x = point[0]
+        y = point[1]
+        f0 = testFunction((x, y - h))
+        f2 = testFunction((x, y + h))
+        dy = (f2 - f0)/(2 * h)
+        return dy
+    def getGradientIn(point):
+        return np.array([numerical_diff_x(point), numerical_diff_y(point)])
+    def getAntiGradientIn(point):
+        g = getGradientIn(point)
+        return -getGradientIn(point)
+    return testFunction, getGradientIn, getAntiGradientIn
+
+x = np.arange(-12, 12, 0.1)
+y = np.arange(-12, 12, 0.1)
+xx, yy = np.meshgrid(x, y)
+f, g, a = getTestData(1e-10)
+xravel = np.ravel(xx)
+yravel = np.ravel(yy)
+points = list(zip(xravel, yravel))
+z = np.array([ f(point) for point in points ])
+zz = z.reshape(xx.shape)
+h = plt.contour(xx, yy, zz, 40)
+
+f = open(filename, 'r')
+vec = []
+vecs = []
+for line in f:
+    trimmed = line.strip()
+    if re.match("^\d+?\.\d*?$", trimmed) is None:
+        if(len(vec)!=0):
+            vecs.append(vec)
+            vec=[]
+    else:
+        number = float(trimmed)
+        vec.append(number)
+
+x_list = [x for [x, y] in vecs]
+y_list = [y for [x, y] in vecs]
+
+plt.plot(x_list, y_list, 'r--')
+plt.show()
--- example.cpp
+++ example.cpp
@@ -14,12 +14,17 @@ PetscErrorCode FormFunctionGradient(Tao,Vec,PetscReal*,Vec,void*);
 int main(int argc,char **argv)
 {
         PetscErrorCode ierr;                /* used to check for functions returning nonzeros */
-        PetscReal zero=0.0;
+        PetscReal zero=0.0, lambda=0.01, *xinitial;
         Vec x;                              /* solution vector */
         Tao tao;                            /* Tao solver context */
         PetscBool flg;
         PetscMPIInt size,rank;                   /* number of processes running */
         AppCtx user;                        /* user-defined application context */
+
+        PetscReal f;
+        Vec G;
+
+        PetscViewer    viewer;
 
         /* Initialize TAO and PETSc */
         ierr = PetscInitialize(&argc,&argv,(char*)0,help); if (ierr) return ierr;
@@ -28,12 +33,13 @@ int main(int argc,char **argv)
         if (size >1) SETERRQ(PETSC_COMM_SELF,1,"Incorrect number of processors");
 
         /* Initialize problem parameters */
-        user.alpha = 99.0;
+        user.alpha = 0.01;
         /* Check for command line arguments to override defaults */
         ierr = PetscOptionsGetReal(NULL,NULL,"-alpha",&user.alpha,&flg); CHKERRQ(ierr);
 
         /* Allocate vectors for the solution and gradient */
         ierr = VecCreateSeq(PETSC_COMM_SELF, 2, &x); CHKERRQ(ierr);
+        ierr = VecCreateSeq(PETSC_COMM_SELF, 2, &G); CHKERRQ(ierr);
 
         /* The TAO code begins here */
         /* Create TAO solver with desired solution method */
@@ -42,6 +48,11 @@ int main(int argc,char **argv)
 
         /* Set solution vec and an initial guess */
         ierr = VecSet(x, zero); CHKERRQ(ierr);
+        ierr = VecSet(G, zero); CHKERRQ(ierr);
+        ierr = VecGetArray(x, &xinitial); CHKERRQ(ierr);
+        xinitial[0]=5.0;
+        xinitial[1]=5.0;
+        ierr = VecRestoreArray(x, &xinitial); CHKERRQ(ierr);
         ierr = TaoSetInitialVector(tao,x); CHKERRQ(ierr);
 
         /* Set routines for function, gradient, hessian evaluation */
@@ -50,11 +61,22 @@ int main(int argc,char **argv)
         /* Check for TAO command line options */
         ierr = TaoSetFromOptions(tao); CHKERRQ(ierr);
 
+        /*  create viewer */
+        ierr = PetscViewerASCIIOpen(PETSC_COMM_WORLD, "example.out", &viewer); CHKERRQ(ierr);
+         PetscViewerPushFormat(viewer, PETSC_VIEWER_ASCII_COMMON);
         /*solver was here*/
-        PetscInfo(NULL, "Matrix uses parameter alpha\n");
-
+        // PetscInfo2(NULL, "position = %g, %g\n",x[0],x[1]);
+        for(int i=0; i<300; i++)
+        {
+                VecView(x, viewer);
+                ierr = FormFunctionGradient(tao, x, &f, G, &user); CHKERRQ(ierr);
+                ierr = VecAXPY(x, -lambda, G); CHKERRQ(ierr);
+        }
         ierr = TaoDestroy(&tao); CHKERRQ(ierr);
         ierr = VecDestroy(&x); CHKERRQ(ierr);
+        ierr = VecDestroy(&G); CHKERRQ(ierr);
+        PetscViewerPopFormat(viewer);
+        ierr = PetscViewerDestroy(&viewer); CHKERRQ(ierr);
 
         ierr = PetscFinalize();
         return ierr;
